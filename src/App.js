6c6f3353c475ef4d737f05598b7a14856a560935
Axel <axstraube@gmail.com>
2024-07-21 13:45:32 -0700

    Add Board and Square

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import './App.css';
 
+function Square() {
+  return <button className="square"></button>;
+}
+
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
+}
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Board />
+    </div>
   );
 }
 
